42f2e207fcc2ae7f83811e73078ce282302c3914
evSnow <evanrodriguez2022@gmail.com>
2024-07-26 20:21:58 -0500

    added the loop to run until someone win 5 times and a evaluration between two value

--- game.js
+++ game.js
@@ -3,12 +3,13 @@ let player=0;
 let playerChoice=0;
 let cpuChoice=0;
 let repeat=1;
+let whoWon=0;
 function randomCpu(){
     return Math.floor(Math.random()*3)+1;
 }
 
 function getPlayerChoice(playerChoice){
-    playerChoice=prompt("plese enter a number 1, 2, 3");
+    playerChoice=prompt("plese enter a number 1:rock , 2:paper, 3:scissors");
     playerChoice=parseInt(playerChoice);
     while(repeat===1){ 
         playerChoice=parseInt(playerChoice);
@@ -22,13 +23,61 @@ function getPlayerChoice(playerChoice){
         repeat=0;
     }
     else{
-        playerChoice=prompt("invalid choice pick between 1-3");
+        playerChoice=prompt("invalid choice pick 1:rock , 2:paper, 3:scissors");
     }
 }
     return playerChoice;
 }
+//1=player  2==cpu   3= none/tie
+function compareChoice(playerChoice,cpuChoice){
+    if(playerChoice === 1 && cpuChoice === 2){
+        return 2;
+    }
+    else if(playerChoice === 1 && cpuChoice === 3){
+        return 1;
+    }
+    else if(playerChoice === 2 && cpuChoice === 1){
+        return 1;
+    }
+    else if(playerChoice === 2 && cpuChoice === 3){
+        return 2;
+    }
+    else if(playerChoice === 3 && cpuChoice === 1){
+        return 2;
+    }
+    else if(playerChoice === 3 && cpuChoice === 2){
+        return 1;
+    }
+    else{
+        return 3;
+    }
+}
 
-
-console.log(randomCpu());
+while(1==1){ 
+cpuChoice=randomCpu();
+                        //console.log(cpuChoice);
 playerChoice=getPlayerChoice(getPlayerChoice);
-console.log(playerChoice);
+                        //console.log(playerChoice);
+whoWon=compareChoice(playerChoice,cpuChoice);
+if(whoWon===1){
+player=player+1;
+alert("the person who won is player" + "      player score: "+player+" cpu score: "+cpu);
+}
+else if(whoWon===2){
+cpu=cpu+1;
+console.log(cpu);
+alert("the person who won is cpu" + "     player score: "+player+" cpu score: "+cpu);
+}
+else if(whoWon===3){
+alert("both choice the same choice");
+if(player===5){ 
+    alert("game is over the winer is player\n Thamks for playing");
+    break;
+    }
+    else if(cpu===5){
+        alert("Game is over and the cpu won\n Thamks for playing")
+        break;
+    }
+}
+
+}
