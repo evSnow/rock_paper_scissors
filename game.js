e95f802dca72f88db97a944b632fe907a7964519
evSnow <evanrodriguez2022@gmail.com>
2024-08-01 02:04:30 -0500

    remove not used function and lines

--- game.js
+++ game.js
@@ -8,26 +8,7 @@ function randomCpu(){
     return Math.floor(Math.random()*3)+1;
 }
 
-function getPlayerChoice(playerChoice){
-    playerChoice=prompt("plese enter a number 1:rock , 2:paper, 3:scissors");
-    playerChoice=parseInt(playerChoice);
-    while(repeat===1){ 
-        playerChoice=parseInt(playerChoice);
-    if(playerChoice===1){
-        repeat=0;
-    }
-    else if(playerChoice===2){
-        repeat=0;
-    }
-    else if(playerChoice===3){
-        repeat=0;
-    }
-    else{
-        playerChoice=prompt("invalid choice pick 1:rock , 2:paper, 3:scissors");
-    }
-}
-    return playerChoice;
-}
+
 //1=player  2==cpu   3= none/tie
 function compareChoice(playerChoice,cpuChoice){
     if(playerChoice === 1 && cpuChoice === 2){
@@ -107,37 +88,3 @@ else if(cpu===5){
 }
 });
 });
-/*
-playerChoice=parseInt(button.id);
-cpuChoice=randomCpu();
-
-whoWon=compareChoice(playerChoice,cpuChoice);
-
-
-
-cpuChoice=randomCpu();
-                        //console.log(cpuChoice);
-playerChoice=getPlayerChoice(getPlayerChoice);
-                        //console.log(playerChoice);
-whoWon=compareChoice(playerChoice,cpuChoice);
-if(whoWon===1){
-player=player+1;
-alert("the person who won is player" + "      player score: "+player+" cpu score: "+cpu);
-}
-else if(whoWon===2){
-cpu=cpu+1;
-console.log(cpu);
-alert("the person who won is cpu" + "     player score: "+player+" cpu score: "+cpu);
-}
-else if(whoWon===3){
-alert("both choice the same choice");
-if(player===5){ 
-    alert("game is over the winer is player\n Thamks for playing");
-    break;
-    }
-else if(cpu===5){
-        alert("Game is over and the cpu won\n Thamks for playing")
-        break;
-    }
-}
-    */
